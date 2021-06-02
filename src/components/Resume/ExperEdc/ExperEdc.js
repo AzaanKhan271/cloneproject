740d1291172bc1ec9cdb29e74d7b07f3aa67fde6
AzaanKhan271 <azaankhan271@gmail.com>
2021-06-02 12:26:33 +0500

    educ and exp of resume completed

--- src/components/Resume/ExperEdc/ExperEdc.js
+++ src/components/Resume/ExperEdc/ExperEdc.js
@@ -1,40 +1,149 @@
 
-import'./experedc.css'
+import './experedc.css'
 
 
 
 
 
 const ExperEdc = () => {
-return(
+  return (
     <>
-    <div className='heading'>
-    <h2 >What I Do</h2>
-    <div id="left_line"></div>
-    <div id="right_line"></div>
-  </div>
-<div className='row'>
+
+
+      <div className='row'>
+        <div className='column' style={{backgroundColor:'transparent',border:'none'}}>
+          <div
+            style={{ marginLeft: '5%', marginRight: '5%', marginTop: '8%' }}
+          >
+            <div className='heading' style={{marginLeft:'-5%'}}>
+              <h2 >Education</h2>
+              <div  id="left_line"></div>
+              <div style={{left:'57%'}} id="right_line"></div>
+
+            </div>
+
+
+            {/* <br /> */}
+            <div 
+            style={{borderLeft:'2px solid #333',borderBottom:'2px solid #333',}}
+            >
+             <div
+             style={{display:'flex',marginLeft:"12%",alignItems:'center',paddingTop:'2%'}}
+             ><h5 className='edcSubHead'>2008</h5><span style={{marginLeft:'2%'}}>University Of Studies</span></div> 
+             <div style={{paddingLeft:'5%',paddingTop:'2%'}}>
+               <h4 style={{fontSize:'16px'}}>UI Design</h4>
+               <p style={{fontSize:'.92em'}}>Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum</p>
+             </div>
+            </div>
+
+            <div 
+            style={{borderLeft:'2px solid #333',borderBottom:'2px solid #333',}}
+            >
+             <div
+             style={{display:'flex',marginLeft:"12%",alignItems:'center',paddingTop:'2%'}}
+             ><h5 className='edcSubHead'>2007</h5><span style={{marginLeft:'2%'}}>University Of Studies</span></div> 
+             <div style={{paddingLeft:'5%',paddingTop:'2%'}}>
+               <h4 style={{fontSize:'16px'}}>Graphic Design</h4>
+               <p style={{fontSize:'.92em'}}>Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum</p>
+             </div>
+            </div>
+            <div 
+            style={{borderLeft:'2px solid #333',borderBottom:'2px solid #333',}}
+            >
+             <div
+             style={{display:'flex',marginLeft:"12%",alignItems:'center',paddingTop:'2%'}}
+             ><h5 className='edcSubHead'>2006</h5><span style={{marginLeft:'2%'}}>University Of Studies</span></div> 
+             <div style={{paddingLeft:'5%',paddingTop:'2%'}}>
+               <h4 style={{fontSize:'16px'}}>Frontend Development</h4>
+               <p style={{fontSize:'.92em'}}>Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum</p>
+             </div>
+            </div>
+            <div 
+            style={{borderLeft:'2px solid #333',borderBottom:'2px solid #333',}}
+            >
+             <div
+             style={{display:'flex',marginLeft:"12%",alignItems:'center',paddingTop:'2%'}}
+             ><h5 className='edcSubHead'>2005</h5><span style={{marginLeft:'2%'}}>University Of Studies</span></div> 
+             <div style={{paddingLeft:'5%',paddingTop:'2%'}}>
+               <h4 style={{fontSize:'16px'}}>MERN Stack</h4>
+               <p style={{fontSize:'.92em'}}>Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum</p>
+             </div>
+            </div>
+
+          </div>
+        </div>
 
 
 
-<div className='column'>
-<h5 className='edcSubHead'>2009</h5><span></span>
-</div>
-  
 
 
 
+        <div className='column' style={{backgroundColor:'transparent',border:'none'}}>
+          <div
+            style={{ marginLeft: '5%', marginRight: '5%', marginTop: '8%' }}
+          >
+            <div className='heading' style={{marginLeft:'-5%'}}>
+              <h2 >Experience</h2>
+              <div  id="left_line"></div>
+              <div style={{left:'10%'}} id="right_line"></div>
+
+            </div>
 
 
-<div className='column'>
+            {/* <br /> */}
+            <div 
+            style={{borderLeft:'2px solid #333',borderBottom:'2px solid #333',}}
+            >
+             <div
+             style={{display:'flex',marginLeft:"12%",alignItems:'center',paddingTop:'2%'}}
+             ><h5 className='edcSubHead'>2016 -  Current</h5><span style={{marginLeft:'2%'}}>Rolling Thunder</span></div> 
+             <div style={{paddingLeft:'5%',paddingTop:'2%'}}>
+               <h4 style={{fontSize:'16px'}}>Lead UI/UX Designer</h4>
+               <p style={{fontSize:'.92em'}}>Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum</p>
+             </div>
+            </div>
+
+            <div 
+            style={{borderLeft:'2px solid #333',borderBottom:'2px solid #333',}}
+            >
+             <div
+             style={{display:'flex',marginLeft:"12%",alignItems:'center',paddingTop:'2%'}}
+             ><h5 className='edcSubHead'>2013 - 2016</h5><span style={{marginLeft:'2%'}}>Locost Accessories</span></div> 
+             <div style={{paddingLeft:'5%',paddingTop:'2%'}}>
+               <h4 style={{fontSize:'16px'}}>Senior UI/UX Designer</h4>
+               <p style={{fontSize:'.92em'}}>Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum</p>
+             </div>
+            </div>
+            <div 
+            style={{borderLeft:'2px solid #333',borderBottom:'2px solid #333',}}
+            >
+             <div
+             style={{display:'flex',marginLeft:"12%",alignItems:'center',paddingTop:'2%'}}
+             ><h5 className='edcSubHead'>2011 - 2013</h5><span style={{marginLeft:'2%'}}>Sagebrush</span></div> 
+             <div style={{paddingLeft:'5%',paddingTop:'2%'}}>
+               <h4 style={{fontSize:'16px'}}>Junior UI/UX Designer</h4>
+               <p style={{fontSize:'.92em'}}>Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum</p>
+             </div>
+            </div>
+            <div 
+            style={{borderLeft:'2px solid #333',borderBottom:'2px solid #333',}}
+            >
+             <div
+             style={{display:'flex',marginLeft:"12%",alignItems:'center',paddingTop:'2%'}}
+             ><h5 className='edcSubHead'>2009 - 2011</h5><span style={{marginLeft:'2%'}}>Tilism Technologies</span></div> 
+             <div style={{paddingLeft:'5%',paddingTop:'2%'}}>
+               <h4 style={{fontSize:'16px'}}>Software Engineer</h4>
+               <p style={{fontSize:'.92em'}}>Duis posuere, quam non imperdiet egestas, eros enim mattis mauris, in posuere lacus arcu quis felis. Etiam interdum erat non enim venenatis fermentum</p>
+             </div>
+            </div>
+
+          </div>
+        </div>
 
 
-</div>
 
-</div>
-
-<div></div>
+      </div>
     </>
-)
+  )
 }
 export default ExperEdc
